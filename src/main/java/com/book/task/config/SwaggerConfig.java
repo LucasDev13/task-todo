aa0f6bd02d05a0c4699db5ce8b44aaee359bc7dc
LucasDev13 <lucaspontes140@gmail.com>
2022-05-21 13:47:19 -0300

    Implementação de testes unitários

--- src/main/java/com/book/task/config/SwaggerConfig.java
+++ src/main/java/com/book/task/config/SwaggerConfig.java
@@ -12,6 +12,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+    /**
+     * Configurações padrões do swagger
+     * @return
+     */
     @Bean
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
